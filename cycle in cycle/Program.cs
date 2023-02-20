a3a96a45989ba16104b2c1e0237d09902d2bcecb
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-20 12:59:43 +0300

    cycle in cycle added

---
+++ cycle in cycle/Program.cs
@@ -0,0 +1,10 @@
+﻿// цикл в цикле
+
+for (int i = 1; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        System.Console.WriteLine($"{i} * {j} = {i*j}");
+    }
+    System.Console.WriteLine(); //вывод пустой строки
+}
