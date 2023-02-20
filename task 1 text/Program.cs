032b7c0a39e1065b24d471afa77c5c12b0289767
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-20 13:22:14 +0300

    task 1 solved

---
+++ task 1 text/Program.cs
@@ -0,0 +1,27 @@
+﻿string text = "Кракозябры стояли стоя стремглав кустами"
+            + "Совсем крабы кость стукнули куском"; 
+// многострочный ввод
+
+// string s = "qwerty"
+//             0123456
+// чтобы получить конкретный символ надо
+// s[3] - выведет e
+
+string Replace (string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length; // вывод длины текста
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}"; //новый вывод if
+        else result = result + $"{text[i]}"; // и else
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '-');
+System.Console.WriteLine(newText);
+newText = Replace(newText, 'с', 'С');
+System.Console.WriteLine(newText);
+newText = Replace(newText, 'к', 'К');
+System.Console.WriteLine(newText);
