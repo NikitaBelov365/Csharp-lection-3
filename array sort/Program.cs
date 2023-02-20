774844ef06dc91a3217d736564afaac058beb8e7
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-20 13:33:38 +0300

    array sort done

---
+++ array sort/Program.cs
@@ -0,0 +1,32 @@
+﻿// сортировка массива
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        System.Console.WriteLine($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1 ; i++)
+    {
+        int minPosition = i;
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
