177cd0daaea96dd7eec5fda7446a9858bf6a7bf5
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-20 12:54:12 +0300

    cycle for inspected

---
+++ cycle for/Program.cs
@@ -0,0 +1,14 @@
+﻿// FOR
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "text");
+System.Console.WriteLine(res);
